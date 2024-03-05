0ce600370cbe198617cb80d650424aa9d630989a
Aaru <{Workingaaru@gmail.com}+{Aaru}@users.noreply.github.com>
2024-03-05 17:37:22 +0530

    v0.2.5 (Update stylesheets and add sidebar and dashboard components)

--- routes/app_route.js
+++ routes/app_route.js
@@ -6,8 +6,8 @@ local();
   
 app.use(express.static('public'));
 app.get('/', authMiddleware,(req, res) => {
-    //res.render("index");
-    res.redirect("/customer/view");
+    res.render("index");
+    //res.redirect("/customer/view");
 });
 app.get('/quote',(req, res) => {
     res.render("invoice/quote");
